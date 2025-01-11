e3f8f0b65e0fb3b885cd6081f6e3d84106adb4bd
AliMecidzade <alimedzidzade@gmail.com>
2025-01-11 17:22:43 +0400

    Fully organized and refactored code
    added the gravity multiplier that wll change if the player is in the air to make better movement mechanics. Added another small changes

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,165 +1,133 @@
 using Godot;
 using System;
-using System.ComponentModel;
 
 public partial class Player : CharacterBody2D
 {
-	[Export] public float Speed = 500.0f;
-	[Export] public float JumpVelocity = -400.0f;
-	[Export] public int MaxJumps = 2;
-	[Export] public float DashSpeed = 50000f; 
-	[Export] public NodePath SpritePath;
-	[Export] public Timer DashDurationTimer;
-	[Export] public Timer DashAgainTimer; 
-	//[Export] public NodePath AnimationPlayerPath;
+    [Export] public float Speed = 500.0f;
+    [Export] public float JumpVelocity = -400.0f;
+    [Export] public int MaxJumps = 2;
+    [Export] public float DashSpeed = 60000f;
+    [Export] public NodePath SpritePath;
+    [Export] public Timer DashDurationTimer;
+    [Export] public Timer DashAgainTimer;    
+    [Export] private float GravityMultiplier = 1.8f;
 
-	private bool _canDash = true;
+
+    private bool _canDash = true;
     private bool _isDashing = false;
-	private Sprite2D _sprite;
-	private AnimationPlayer _animationPlayer;
-	private float _gravity;
-	private int _jumpsRemaining;
-	private int _dashesRemaining;
-	public override void _Ready()
-	{
-		InitializeComponents();
-		InitializeVariables();
-	}
-	
-	public override void _PhysicsProcess(double delta)
-	{
-		var velocity = Velocity;
+    private Sprite2D _sprite;
+    private float _gravity;
+    private int _jumpsRemaining;
 
-		ApplyGravity(ref velocity, delta);
-		HandleMovement(ref velocity, delta);
-		HandleJump(ref velocity);
-		HandleDash(ref  velocity , delta); 
-		//UpdateAnimations(velocity);
+    
+    private const string DashAction = "dash";
+    private const string JumpAction = "ui_accept";
 
-		Velocity = velocity;
-		MoveAndSlide();
-	}
-
-	private void InitializeComponents()
-	{
-		_gravity = (float)ProjectSettings.GetSetting("physics/2d/default_gravity");
-
-		if (!string.IsNullOrEmpty(SpritePath))
-		{
-			_sprite = GetNode<Sprite2D>(SpritePath);
-		}
-		else
-		{
-			GD.PushError("SpritePath is not set!");
-		}
-
-		//if (!string.IsNullOrEmpty(AnimationPlayerPath))
-		//{
-		//    _animationPlayer = GetNode<AnimationPlayer>(AnimationPlayerPath);
-		//}
-	}
-
-	private void InitializeVariables()
-	{ 
-       
-        _jumpsRemaining = MaxJumps;
-		
-	  
-	}
-
-	private void ApplyGravity(ref Vector2 velocity, double delta)
-	{
-		if (!IsOnFloor())
-		{
-			velocity.Y += _gravity * (float)delta;
-		}
+    public override void _Ready()
+    {
+        Initialize();
     }
 
+    public override void _PhysicsProcess(double delta)
+    {
+        var velocity = Velocity;
+
+        ApplyGravity(ref velocity, delta);
+        HandleMovement(ref velocity, delta);
+        HandleJump(ref velocity);
+        HandleDash(ref velocity, delta);
+
+        Velocity = velocity;
+        MoveAndSlide();
+    }
+
+    private void Initialize()
+    {
+        _gravity = (float)ProjectSettings.GetSetting("physics/2d/default_gravity");
+
+        if (!string.IsNullOrEmpty(SpritePath))
+        {
+            _sprite = GetNode<Sprite2D>(SpritePath);
+        }
+        else
+        {
+            GD.PushError("SpritePath is not set!");
+        }
+
+        _jumpsRemaining = MaxJumps;
+    }
+
+    private void ApplyGravity(ref Vector2 velocity, double delta)
+    {
+        if (!IsOnFloor())
+        {
+            velocity.Y += GravityMultiplier * _gravity * (float)delta;
+        }
+    }
 
     private void OnDashTimerTimeOut()
     {
         _isDashing = false;
     }
 
-	private void OnDashAgainTimerTimeout()
-	{
+    private void OnDashAgainTimerTimeout()
+    {
         _canDash = true;
     }
 
     private void HandleDash(ref Vector2 velocity, double delta)
     {
-
         var dashDirection = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down").Normalized();
-        if (Input.IsActionJustPressed("dash") && _canDash)
+        if (Input.IsActionJustPressed(DashAction) && _canDash)
         {
             _isDashing = true;
             _canDash = false;
             DashDurationTimer.Start();
-			DashAgainTimer.Start();
+            DashAgainTimer.Start();
         }
 
         if (_isDashing)
         {
             velocity.X += dashDirection.X * DashSpeed * (float)delta;
         }
-
-        
     }
 
-    // REFACTOR THIS SHIT
-	// fuck this 
-
-
     private void HandleMovement(ref Vector2 velocity, double delta)
-	{
-		var inputDirection = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+    {
+        var inputDirection = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 
-		if (inputDirection.X != 0)
-		{
-			velocity.X = inputDirection.X * Speed;
-			_sprite.FlipH = inputDirection.X < 0;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(velocity.X, 0, Speed * (float)delta * 1.5f);
-		}
-	}
+        if (inputDirection.X != 0)
+        {
+            velocity.X = inputDirection.X * Speed;
+            _sprite.FlipH = inputDirection.X < 0;
+        }
+        else
+        {
+            velocity.X = Mathf.MoveToward(velocity.X, 0, Speed * (float)delta * 1.5f);
+        }
+    }
 
-	private void HandleJump(ref Vector2 velocity)
-	{
-		if (IsOnFloor())
-		{
-			_jumpsRemaining = MaxJumps;
-		}
+    private void HandleJump(ref Vector2 velocity)
+    {
+        if (IsOnFloor())
+        {
+            _jumpsRemaining = MaxJumps;
+        }
+        else
+        {
+            GravityMultiplier = 2.3f; 
+        }
 
-		if (Input.IsActionJustPressed("ui_accept") && _jumpsRemaining > 0)
-		{
-			velocity.Y = JumpVelocity;
-			_jumpsRemaining--;
-		}
-	}
+        if (Input.IsActionJustPressed(JumpAction) && _jumpsRemaining > 0)
+        {
+            velocity.Y = JumpVelocity;
+            _jumpsRemaining--;
+        }
+    }
 
-
-	// Add Crouching 
-	// Refactor the code 
-	
-	// i hate my life 
-
-	//private void UpdateAnimations(Vector2 velocity)
-	//{
-	//    if (_animationPlayer == null) return;
-
-	//    if (!IsOnFloor())
-	//    {
-	//        _animationPlayer.Play("jump");
-	//    }
-	//    else if (Mathf.Abs(velocity.X) > 0)
-	//    {
-	//        _animationPlayer.Play("run");
-	//    }
-	//    else
-	//    {
-	//        _animationPlayer.Play("idle");
-	//    }
-	//}
+    private void OnDeathAreaEntered(CharacterBody2D player)
+    {
+        GD.Print("You Died");
+        QueueFree();
+    }
 }
